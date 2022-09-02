e90dc7c89c66d1091deab787403b264de0dc61f1
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-09-03 04:13:44 +0600

    api link added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const loadNewCategories = async () => {
+  const url = 'https://openapi.programming-hero.com/api/news/categories';
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+};
