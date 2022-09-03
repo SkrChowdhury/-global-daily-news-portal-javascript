d06a41d4c1fd9085b6cef9c2fa6ef2a157a84a47
SkrChowdhury <sakkhor.chowdhury@gmail.com>
2022-09-03 15:31:29 +0600

    loader added

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const displayCategories = (categories) => {
     categoriesLi.classList.add('nav-item');
 
     categoriesLi.innerHTML = `
-        <a onclick="loadNews('${category.category_id}')" class="nav-link active text-white rounded-pill" href="#"
+        <a onclick="loadNews('${category.category_id}')" class="nav-link text-white rounded-pill" href="#"
               >${category.category_name}</a
         >
     `;
@@ -31,18 +31,24 @@ const loadNews = (category_id) => {
 };
 
 const displayNews = (newsCards) => {
+  toggleSpinner(true);
   const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = ``;
+
   newsCards.forEach((card) => {
-    cardContainer.innerHTML = `
+    const cardDiv = document.createElement('div');
+    cardDiv.innerHTML = `
         <div class="card mb-3 shadow-lg rounded">
             <div class="row g-0">
               <div class="col-md-4">
-                <img src="" class="img-fluid rounded-start" alt="..." />
+                <img src="${
+                  card.image_url
+                }" class="img-fluid rounded-5 m-3" alt="..." />
               </div>
               <div class="col-md-8">
                 <div class="card-body m-3">
                   <h5 class="card-title">${card.title}</h5>
-                  <p class="card-text">
+                  <p class="card-text text-justify">
                     ${card.details.slice(0, 400) + '...'}
                   </p>
                   <div
@@ -75,15 +81,24 @@ const displayNews = (newsCards) => {
                       <button class="btn btn-danger rounded-pill" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                         Details
                       </button>
-                      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+                      <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                           <div class="modal-content">
                             <div class="modal-header">
-                              <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                              <h5 class="modal-title text-danger fw-bold" id="staticBackdropLabel">${
+                                card.title
+                              }</h5>
                               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                              ...
+                              <div>
+
+                              <img
+                        class="rounded-circle w-25 h-25"
+                        src="${card.author.img}"
+                        alt=""
+                      />
+                              </div>
                             </div>
                             <div class="modal-footer">
                               <button type="button" class="btn btn-danger rounded-pill" data-bs-dismiss="modal">Close</button>
@@ -98,9 +113,24 @@ const displayNews = (newsCards) => {
             </div>
           </div>
     `;
+
+    cardContainer.appendChild(cardDiv);
+
+    const newsCountText = document.getElementById('news-count');
+    newsCountText.innerText = `${newsCards.length} items found for the category `;
   });
+  //stop spinner
+  toggleSpinner(false);
+};
+
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById('loader');
+  if (isLoading) {
+    loaderSection.classList.remove('d-none');
+  } else {
+    loaderSection.classList.add('d-none');
+  }
 };
 
 loadCategories();
-
 loadNews();
